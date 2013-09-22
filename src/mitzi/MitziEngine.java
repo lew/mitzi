1e70b337729f93896b2cfdb6d85da5824d1e5769
Stefan Lew <admin@stefanlew.com>
2013-09-22 11:27:33 +0200

    main file

---
+++ src/mitzi/MitziEngine.java
@@ -0,0 +1,13 @@
+package mitzi;
+
+public class MitziEngine {
+
+	public static void main(String[] args) {
+
+		System.out.println("id name Mitzi 0.1");
+		System.out.println("id author Ch. Hofer, S. Lew");
+		System.out.println("unexpected error. mitzi died.");
+
+	}
+
+}
