143bdd156577a38fa7319bb4c5a9a82b63e35c8a
Stefan Lew <admin@stefanlew.com>
2013-09-23 13:55:09 +0200

    extended IBoard

--- src/mitzi/IBoard.java
+++ src/mitzi/IBoard.java
@@ -1,6 +1,6 @@
 package mitzi;
 
-import java.util.List;
+import java.util.Set;
 
 public interface IBoard {
 
@@ -9,7 +9,35 @@ public interface IBoard {
 	public void setToFEN(String fen);
 
 	public IBoard doMove(IMove move);
+	
+	public boolean canEnPassant(int hit_field);
+	
+	public boolean canRochade(int king_to);
 
-	public List<IMove> getPossibleMoves();
+	public Set<Integer> getOccupiedSquaresByColor(int color);
+
+	public Set<Integer> getOccupiedSquaresByType(int type);
+
+	public Set<Integer> getOccupiedSquaresByColorAndType(int color, int type);
+
+	public int getNumberOfFiguresByColor(int color);
+
+	public int getNumberOfFiguresByType(int type);
+
+	public int getNumberOfFiguresByColorAndType(int color, int type);
+
+	public Set<IMove> getPossibleMoves();
+
+	public Set<IMove> getPossibleMovesFrom();
+
+	public Set<IMove> getPossibleMovesTo();
+
+	public boolean isCheckPosition();
+
+	public boolean isMatePosition();
+
+	public boolean isPossibleMove(IMove move);
+
+	public String toFEN();
 
 }
