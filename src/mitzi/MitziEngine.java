1b43da296db1a04a1b3f9c6fa41b29b12953f2f1
Stefan Lew <admin@stefanlew.com>
2013-09-23 09:52:39 +0200

    full names

--- src/mitzi/MitziEngine.java
+++ src/mitzi/MitziEngine.java
@@ -5,7 +5,7 @@ public class MitziEngine {
 	public static void main(String[] args) {
 
 		System.out.println("id name Mitzi 0.1");
-		System.out.println("id author Ch. Hofer, S. Lew");
+		System.out.println("id author Christoph Hofer, Stefan Lew");
 		System.out.println("uciok");
 		System.exit(0);
 
