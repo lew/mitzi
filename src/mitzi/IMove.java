db5d44f32957d01264d8c481c28bd79b1c0ee9ba
DaHofa <hofer.christoph@liwest.at>
2013-09-24 20:50:27 +0200

    finished IMove

--- src/mitzi/IMove.java
+++ src/mitzi/IMove.java
@@ -1,11 +1,25 @@
 package mitzi;
 
 public interface IMove {
-	
+
+	/**
+	 * 
+	 * @return the source of the move
+	 */
 	public int getFromSquare();
-	
+
+	/**
+	 * 
+	 * @return the destination of the move
+	 */
 	public int getToSquare();
-	
+
+	/**
+	 * 
+	 * @return the promotion of the pawn. 0 if no promotion.
+	 */
+	public int getPromotion();
+
 	public String toString();
 
 }
