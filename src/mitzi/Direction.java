17c56de4c7e13fc9479a29f080dfa24b3f2758b3
Stefan Lew <admin@stefanlew.com>
2013-09-26 21:50:07 +0200

    documentation added

--- src/mitzi/Direction.java
+++ src/mitzi/Direction.java
@@ -4,7 +4,19 @@ public enum Direction {
 	EAST(10, 21), NORTHEAST(11, 12), NORTH(1, -8), NORTHWEST(-9, -19), WEST(
 			-10, -21), SOUTHWEST(-11, -12), SOUTH(-1, 8), SOUTHEAST(9, 19);
 
+	/**
+	 * Add to a square value to go one step in the specified direction.
+	 * 
+	 * White is South, Black is North.
+	 */
 	public final int offset;
+
+	/**
+	 * Add to a square value to go one knight-step in the specified direction.
+	 * 
+	 * One up and two right is East. Two up one right is Northeast. Basically,
+	 * the orientation is shifted a bit counterclockwise.
+	 */
 	public final int knight_offset;
 
 	Direction(int offset, int knight_offset) {
