c6495468a6c1800d690dd1953495d644a0c573f0
Stefan Lew <admin@stefanlew.com>
2013-09-27 10:08:20 +0200

    make directions color-aware

--- src/mitzi/Direction.java
+++ src/mitzi/Direction.java
@@ -1,5 +1,7 @@
 package mitzi;
 
+import java.util.EnumSet;
+
 public enum Direction {
 	EAST(10, 21), NORTHEAST(11, 12), NORTH(1, -8), NORTHWEST(-9, -19), WEST(
 			-10, -21), SOUTHWEST(-11, -12), SOUTH(-1, 8), SOUTHEAST(9, 19);
@@ -22,6 +24,38 @@ public enum Direction {
 	Direction(int offset, int knight_offset) {
 		this.offset = offset;
 		this.knight_offset = knight_offset;
+	}
+
+	/**
+	 * Returns the direction in which a pawn of the specified color can move
+	 * (without capturing).
+	 * 
+	 * @param color
+	 *            the color of the piece
+	 * @return NORTH for white and SOUTH for black
+	 */
+	public static Direction pawnDirection(int color) {
+		if (color == PieceHelper.WHITE) {
+			return NORTH;
+		} else {
+			return SOUTH;
+		}
+	}
+
+	/**
+	 * Returns a set of directions in which a pawn of the specified color can
+	 * capture other pieces.
+	 * 
+	 * @param color
+	 *            the color of the piece
+	 * @return the set of directions allowed
+	 */
+	public static EnumSet<Direction> pawnCapturingDirections(int color) {
+		if (color == PieceHelper.WHITE) {
+			return EnumSet.of(NORTHEAST, NORTHWEST);
+		} else {
+			return EnumSet.of(SOUTHEAST, SOUTHWEST);
+		}
 	}
 
 }
