092ca68116980e3478ad0b14e3afb1b2e04242d9
Stefan Lew <admin@stefanlew.com>
2013-11-03 14:38:29 +0100

    consider bishop pair as bonus

--- src/mitzi/PrimitiveBoardAnalyzer.java
+++ src/mitzi/PrimitiveBoardAnalyzer.java
@@ -4,23 +4,29 @@ public class PrimitiveBoardAnalyzer implements IBoardAnalyzer {
 
 	@Override
 	public AnalysisResult eval0(IBoard board) {
-		
+
 		int score = 0;
 
-		int[] fig_value = { 100, 500, 325, 325, 975, 000 };
+		int[] piece_values = { 100, 500, 325, 325, 975, 000 };
+		int bishop_pair_value = 50;
 
-		// Maybe not the most efficient way (several runs over the board)
-		for (Side c : Side.values()) {
-			int side_sign = Side.getSideSign(c);
-			for (Piece fig : Piece.values()) {
-				score += board.getNumberOfPiecesByColorAndType(c, fig)
-						* fig_value[fig.ordinal()] * side_sign;
+		// basic evaluation
+		for (Side side : Side.values()) {
+			int side_sign = Side.getSideSign(side);
+
+			// piece values
+			for (Piece piece : Piece.values()) {
+				score += board.getNumberOfPiecesByColorAndType(side, piece)
+						* piece_values[piece.ordinal()] * side_sign;
 			}
 
+			// bishop pair gives bonus
+			if (board.getNumberOfPiecesByColorAndType(side, Piece.BISHOP) == 2) {
+				score += bishop_pair_value * side_sign;
+			}
 		}
 
 		AnalysisResult result = new AnalysisResult(score, false, false);
 		return result;
 	}
-
 }
